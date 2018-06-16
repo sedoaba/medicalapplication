59a2b37c2dbbf3f822ad551414df15b63f80f6e8
Lebohang <lebza.sedoaba@gmail.com>
2018-06-16 11:36:00 +0200

    Testing login

--- handler/config.php
+++ handler/config.php
@@ -1,7 +1,7 @@
 <?php
 
 
-	$conn = new mysqli("127.0.0.1", "root", "", "10ant") or die("Error connecting to database");
+	$conn = new mysqli("173.212.193.152", "unite001_admin", "unitech@2018", "unite001_dr_db") or die("Error connecting to database");
 	//$conn = new mysqli("127.0.0.1", "root", "", "10ant") or die("Error connecting to database");
 
 ?>
